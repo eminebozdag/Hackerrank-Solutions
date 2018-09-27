b4b5155e9e839ea6e9995cc18a3284e0d9dc6b3d
feyzasrgll <34144604+feyzasrgll@users.noreply.github.com>
2018-09-27 18:50:07 +0300

    Diagonal Difference
    Run the Project in Visual Studio.

---
+++ Algorithms/Warmup/DiagonalDifference.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i,j;
+            int sum1 = 0;
+            int sum2 = 0;
+
+            int[,] ar = new int[n,n];
+            for (i = 0; i <n; i++) //enter number
+            {
+                for ( j = 0; j < n; j++)
+                {
+                    ar[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (i = 0; i < n;  i++) //read number
+            {
+                 for ( j = 0; j < n; j++)
+                {
+                    //Console.WriteLine(array[i, j]); 
+                    if (i == j)
+                    {
+                        sum1 += ar[i, j];                        
+                    }                    
+                }
+            }
+            sum2 = ar[0, 2] + ar[1, 1] + ar[2, 0];
+            int tot = sum1 - sum2;
+            if (tot < 0)
+
+                Console.WriteLine(tot * -1);
+            else
+                Console.WriteLine(tot);
+
+            Console.ReadLine();
+        }
+    }
+}
